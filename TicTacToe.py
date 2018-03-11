c3a49673d5b920dd2a4fe055ba0b10c851479ca1
Henry440 <440henry@gmail.com>
2018-03-11 11:03:44 +0100

    Code Schlanker gestaltet

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,57 +6,53 @@ board = [0, 1, 2,
          3, 4, 5,
          6, 7, 8]
 
-player = "x"
-computer = "o"
+player = str(input("Playerfigur : "))
+computer = str(input("Computerfigur : "))
 figur = ""
 
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 def play_game():
 
     playing = True
     
     while playing:
-        
-        print(board[0], "|", board[1], "|", board[2])
-        print("----------")
-        print(board[3], "|", board[4], "|", board[5])
-        print("----------")
-        print(board[6], "|", board[7], "|", board[8])
-        print("")
-        
+        cls()
+        get_board()
         print("Player bitte Feld wählen:")
         player_set = int(input("> "))
 
         if board[player_set] != player and computer:
             board[player_set] = player
             checkwin(board, player)
-
         else:
+            cls()
             get_board()
-            
             print("Player bitte Feld wählen:")
             player_set = int(input("> "))
 
             if board[player_set] != player and computer:
                 board[player_set] = player
                 checkwin(board, player)
-
             else:
                 print("PC ist am Zug")
+
         think = True
-        
         while think:
             random.seed()
             computer_set = random.randint(0,8)
             if board[computer_set] != player and computer:
                 board[computer_set] = computer
                 checkwin(board, computer)
-                #get_board()
+                cls()
+                get_board()
                 break
 
 def checkwin(board, figur):
-    print(board)
     figur = figur
+    
     if board[0] == figur:
         if board[1] == figur:
             if board[2] == figur:
@@ -130,7 +126,7 @@ def checkwin(board, figur):
                 else:
                     print("Der Computer hat gewonnen")
                     end_screen()
-        
+    
     
 def get_board():
     print("Feld schon Belegt \nBitte nochmal eingeben")
