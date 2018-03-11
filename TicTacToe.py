c9b9dcb5d79d21df274db387977ef9cf530d9650
Henry440 <440henry@gmail.com>
2018-03-11 11:12:39 +0100

    Vertige Version ohne Bugs Wiederholtes Spielen möglich AI des Computers beruht auf zufälligkeit AI Computer wird in zukunft erschwert

--- TicTacToe.py
+++ TicTacToe.py
@@ -138,13 +138,24 @@ def get_board():
     print("")
 
 def end_screen():
-    time.sleep(10)
+    time.sleep(3)
+    cls()
+    print("")
+    print("Erneut Spielen ? [y] Ja [n] Nein")
+    auswahl = str(input("> "))
+    if auswahl == "y":
+        reset()
+    else:
+        exit()
+    
+def reset():
+    global board
     board = [0, 1, 2,
          3, 4, 5,
          6, 7, 8]
+    player = str(input("Playerfigur : "))
+    computer = str(input("Computerfigur : "))
+    play_game()
     
-    
-    exit()
-    
-
+figur = ""
 play_game()
